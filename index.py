12e7f5fe386c590bc6834b49b0803121ab14fa51
Elias Severiano Amaral <eliasseverianoamaral@gmail.com>
2024-06-10 22:02:35 -0300

    tratativa de erros

--- index.py
+++ index.py
@@ -3,19 +3,22 @@ def conte_palavra(frase):
     print(f"Número de palavras: {len(palavras)}")
 
 def conte_caracter(frase):
-    caracteres = len(frase)
+    caracteres = len(frase.replace(" ", ""))
     print(f"Número de caracteres: {caracteres}")
 
 while True:
     frase = input("Digite uma frase: ")
-    opcao = int(input("Selecione uma opção: 1 - Contar palavras 2 - Contar caracteres 3 - Sair\n"))
-    
-    if opcao == 1:
-        conte_palavra(frase)
-    elif opcao == 2:
-        conte_caracter(frase)
-    elif opcao == 3:
-        print("Saindo...")
-        break
-    else:
+    opcao = input("Selecione uma opção: 1 - Contar palavras 2 - Contar caracteres 3 - Sair\n")
+
+    try:
+        if opcao == 1:
+            conte_palavra(frase)
+        elif opcao == 2:
+            conte_caracter(frase)
+        elif opcao == 3:
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")
+    except: 
         print("Opção inválida.")
