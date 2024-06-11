15c80d6f7c5f2ba5dcb706906026b4794a5aa5ee
Adenilson <sradenilson.sr16@gmail.com>
2024-06-11 16:34:40 -0300

    Alterando arquivo contarCaracteres.py

---
+++ contarCaracteres.py
@@ -0,0 +1,42 @@
+# REVISANDO 
+
+# Declaração de varíaveis
+nome = "Adenilson";
+idade = 22;
+altura = 1.70;
+solteiro = True;
+letra = 'a';
+listaNomes = {"Adenilson", "Lucas", "Pedro"};
+
+# Operadores aritméticos
+
+# Adição +
+# Subtração -
+# Multiplicação *
+# Divisão /
+# Resto %
+
+# Operadores de comparação
+ 
+# Menor que <
+# Maior que >
+# Menor ou igual <=
+# Maior ou igual >=
+# Igual ==
+# Diferente !=
+
+# Operadores lógicos
+
+# and
+# or
+# not
+
+num1 = 2;
+num2 = 6;
+
+if(num1 < num2):
+    print("O " + str(num1) + " é menor que " + str(num2));
+elif(num1 >= num2):
+    print("O " + str(num1) + " é maior ou igual a " + str(num2));
+else:
+    print("UNKNOW")
