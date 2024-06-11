1eb42ad7262bf3f0905b4f567805a5207dfd28a9
Adenilson <sradenilson.sr16@gmail.com>
2024-06-11 16:38:30 -0300

    Alterando arquivo contarCaracteres.py

--- contarCaracteres.py
+++ contarCaracteres.py
@@ -1,42 +1,26 @@
-# REVISANDO 
+def contarCaracteres():
+    
+    executando = True;
+    
+    try:
+        while(executando):
+            
+            opcao = int(input("\nO que você deseja fazer:\n\n1 - Contar caracteres de uma palavra\n2 - Contar palavras de um texto\n3 - Sair do programa\n\n>>> "));
 
-# Declaração de varíaveis
-nome = "Adenilson";
-idade = 22;
-altura = 1.70;
-solteiro = True;
-letra = 'a';
-listaNomes = {"Adenilson", "Lucas", "Pedro"};
-
-# Operadores aritméticos
-
-# Adição +
-# Subtração -
-# Multiplicação *
-# Divisão /
-# Resto %
-
-# Operadores de comparação
- 
-# Menor que <
-# Maior que >
-# Menor ou igual <=
-# Maior ou igual >=
-# Igual ==
-# Diferente !=
-
-# Operadores lógicos
-
-# and
-# or
-# not
-
-num1 = 2;
-num2 = 6;
-
-if(num1 < num2):
-    print("O " + str(num1) + " é menor que " + str(num2));
-elif(num1 >= num2):
-    print("O " + str(num1) + " é maior ou igual a " + str(num2));
-else:
-    print("UNKNOW")
+            if(opcao == 1):
+                texto = input("Informe uma palavra: ");
+                totalCaracteres = len(texto.replace(" ",""));
+                print("A palavra fornecida possui " + str(totalCaracteres) + " caracteres");
+            
+            elif(opcao == 2):
+                texto = input("Informe um texto: ");
+                palavras = texto.split();
+                print("O texto informado possui " + str(len(palavras)) + " palavras");
+            elif(opcao == 3):
+                executando = False
+                print("Fechando programa...");
+            else:
+                print("\nDigite uma opção válida");
+    except: 
+        print("\nInformação inválida, tente novamente\n");   
+contarCaracteres();
